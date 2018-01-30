32334f4c35f90be0bc25fe91d12214c8c5260e65
jhavens <jhavens12@gmail.com>
2018-01-30 09:32:47 -0500

    Includes Running + Totals
    Combined to single script which prompts if you want  running days or total  days calculated

--- Month_Compare_Total.py
+++ Month_Compare_Total.py
@@ -28,6 +28,36 @@ def graph_per_month_total(master_dict,choice_dict):
     plt.legend()
     plt.show()
 
+def graph_per_month_running(master_dict,choice_dict):
+    #elapsed_time, distance_miles, average_speed, kudos_count, max_heartrate, average_heartrate, max_speed, pace_dec, total_elevation_gain,
+    #athlete_count, average_temp, achivement_count
+    input1 = 7
+    input2 = 'distance_miles'
+
+    graph_dict = {}
+    for choice in choice_dict:
+        graph_dict[choice] = calc.full_running_totals(master_dict.copy(),input1,input2)
+
+    for date_range,choice in zip(graph_dict,choice_dict):
+        for key in list(graph_dict[date_range].keys()):
+
+            if key < get_time.FOM(choice_dict[choice]) or key > get_time.LOM(choice_dict[choice]):  #
+                del graph_dict[date_range][key]
+
+        y_list = []
+        for key in sorted(graph_dict[date_range].keys()):
+            y_list.append(graph_dict[date_range][key])
+        x_list = list(range(1,len(list(graph_dict[date_range].keys()))+1))
+
+        plt.plot(x_list,y_list,label=(get_time.what_month(get_time.FOM(choice_dict[choice]).month)+" "+str(get_time.FOM(choice_dict[choice]).year)))
+
+    plt.style.use('dark_background')
+    plt.rcParams['lines.linewidth'] = 1
+    plt.ylim(ymin=0)
+    plt.title('Running Total - ' + str(input1) + ' days, Unit: ' + input2)
+    plt.legend()
+    plt.show()
+
 list1 = list(range(0,15)) #past 15 months
 list2 = []
 for x in list1:
@@ -37,10 +67,12 @@ for x,y in zip(list1,list2):
     print(str(x)+" - "+str(y))
 
 q1 = int(input("How many months would you like to graph? "))
+q2 = int(input("Running Graph or Total? \n 1 - Total \n 2 - Running \n"))
 
 choice_dict = {}
 for q in list(range(1,q1+1)):
     choice_dict[q] = int(input("What is the number "+str(q)+" month number? "))
-
-pprint(choice_dict)
-graph_per_month_total(master_dict,choice_dict)
+if q2 == 1:
+    graph_per_month_total(master_dict,choice_dict)
+if q2 == 2:
+    graph_per_month_running(master_dict,choice_dict)
