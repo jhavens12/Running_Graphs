8e15ed6b2bd3b67f0d5682523045948c1f5e7bf5
jhavens <jhavens12@gmail.com>
2018-05-25 14:02:11 -0400

    adjustments to graphs

--- Email.py
+++ Email.py
@@ -33,7 +33,7 @@ graph_list = []
 master_dict = get_data.my_filtered_activities()
 
 def append_image(graph_name,plt):
-    name = './temp/'+graph_name+'.png'
+    name = './temp/'+graph_name+'.png' #this needs to change if run by cron
     plt.savefig(name)
     graph_list.append(name)
 
@@ -99,23 +99,23 @@ def weekly_compare():
                 if 'total_elevation_feet' in week_dict[week][activity]:
                     ele_list.append(float(week_dict[week][activity]['total_elevation_feet']))
                     ele_dict[get_time.LM(week)] = sum(ele_list)#/len(ele_list)
-                else:
-                    ele_dict[get_time.LM(week)] = 0
+                # else:
+                #     ele_dict[get_time.LM(week)] = 0
                 if 'treadmill_flagged' in week_dict[week][activity]:
                     if week_dict[week][activity]['treadmill_flagged'] == 'yes':
                         tred_list.append(1)
-                else:
-                    tred_list.append(0)
+                # else:
+                #     tred_list.append(0)
             hr_dict[get_time.LM(week)] = sum(hr_list)/len(hr_list)
             miles_dict[get_time.LM(week)] = sum(mile_list)
             pace_dict[get_time.LM(week)] = sum(pace_list)/len(pace_list)
             tred_dict[get_time.LM(week)] = sum(tred_list)
             count_dict[get_time.LM(week)] = sum(count_list)
-        else:
-            miles_dict[get_time.LM(week)] = 0
-            pace_dict[get_time.LM(week)] = 0
-            hr_dict[get_time.LM(week)] = 0
-            count_dict[get_time.LM(week)] = 0
+        # else:
+        #     miles_dict[get_time.LM(week)] = 0
+        #     pace_dict[get_time.LM(week)] = 0
+        #     hr_dict[get_time.LM(week)] = 0
+        #     count_dict[get_time.LM(week)] = 0
 
     x_list = []
     y_list = []
@@ -153,51 +153,80 @@ def weekly_compare():
         x6_list.append(month)
         y6_list.append(count_dict[month])
 
-    fig, (ax1,ax2,ax4,ax5) = plt.subplots(nrows=4, figsize=mass_figsize) #figsize sets window
-
+    ########
+    fig, (ax1,ax2,ax4,ax5) = plt.subplots(nrows=4, figsize=(13,8)) #figsize sets window
 
     myFmt = mdates.DateFormatter('%m/%d')
 
     ax1df = trend_line(x_list, y_list)
-    ax1.bar(x_list, y_list, align='center', width=6)
+    ax1.bar(x_list, y_list, align='center', width=6, label="Total: "+format_number(sum(y_list)))
     ax1slope = format_number(float(ax1df['y_trend'].iloc[0]) - float(ax1df['y_trend'].iloc[-1]))
     ax1.plot_date(ax1df.dates, ax1df.y_trend, 'red', ls='--', marker='None',label=ax1slope)
     ax1.set_ylabel('Miles Ran', color='b')
     ax1.set_yticks(range(int(max(y_list))+1),3)
+
+    ax1.set_xticks(x_list)
+
     ax1.tick_params('y', colors='b')
     ax1.yaxis.grid(True)
     ax1.legend()
-    ax1.set_xticks(x_list)
+
+    labels = ax1.set_xticklabels(x_list)
+    for i, label in enumerate(labels):
+        label.set_y(label.get_position()[1] - (i % 2) * 0.09)
+
     ax1.xaxis.set_major_formatter(myFmt)
 
-    ax2.plot(x2_list,y2_list, color='g', label='Pace', linewidth=2)
+    ####
+    ax2.plot(x2_list,y2_list, color='g', linewidth=2, label='Pace: '+format_number(sum(y2_list)/len(y2_list)))
     ax2.set_ylabel('Pace (Decimal)', color='g')
     ax2.tick_params('y', colors='g')
     ax2.yaxis.grid(True)
+    ax2.set_xticks(x2_list)
+    labels = ax2.set_xticklabels(x2_list)
+    for i, label in enumerate(labels):
+        label.set_y(label.get_position()[1] - (i % 2) * 0.09)
+    ax2.xaxis.set_major_formatter(myFmt)
+    ax2.legend()
+
     ax3 = ax2.twinx()
     ax3.plot(x3_list,y3_list, color='r', label='Avg HR')
     ax3.set_ylabel('Avg of Avg HR', color='r')
     ax3.tick_params('y', colors='r')
-    ax2.set_xticks(x2_list)
-    ax2.xaxis.set_major_formatter(myFmt)
 
+
+    #Outside/partner/treadmill
     ax4.bar(x6_list,y6_list, align='center', width=6, color='b', label='Outdoor') #total runs
-    ax4.bar(x5_list,y5_list, align='center', width=6, color='#fc5e02', label='Treadmill') #treadmill runs
+    ax4.bar(x5_list,y5_list, align='center', width=6, color='#fc5e02', label=('Treadmill: '+str(sum(y5_list)))) #treadmill runs
+    ax4.bar(x7_list,y7_list, align='center', width=6, color='#f442e2', label=('Partner: '+str(sum(y7_list)))) #treadmill runs
     ax4.set_ylabel('Runs Per Week', color='b')
     ax4.set_yticks(range(max(y6_list)+1))
+    ax4.set_xticks(x5_list)
+    labels = ax4.set_xticklabels(x5_list)
+    for i, label in enumerate(labels):
+        label.set_y(label.get_position()[1] - (i % 2) * 0.09)
+    ax4.xaxis.set_major_formatter(myFmt)
     ax4.tick_params('y', colors='b')
     ax4.yaxis.grid(True)
     ax4.legend()
-    ax4.set_xticks(x5_list)
-    ax4.xaxis.set_major_formatter(myFmt)
 
-    ax5.plot(x4_list,y4_list, label='Total')
+    #elevation
+    ax5.plot(x4_list,y4_list, label='Total: '+format_number(sum(y4_list)))
     ax5.set_ylabel('Total Elevation (Feet)')
     #ax5.set_yticks(range(int(max(y4_list)+1)),20)
+    ax5.set_xticks(x4_list)
+    labels = ax5.set_xticklabels(x4_list)
+    for i, label in enumerate(labels):
+        label.set_y(label.get_position()[1] - (i % 2) * 0.09)
+    ax5.xaxis.set_major_formatter(myFmt)
     ax5.yaxis.grid(True)
     ax5.legend()
-    ax5.set_xticks(x4_list)
-    ax5.xaxis.set_major_formatter(myFmt)
+
+    plt.setp(ax1.get_xticklabels(), rotation=0, horizontalalignment='center')
+    plt.setp(ax2.get_xticklabels(), rotation=0, horizontalalignment='center')
+    plt.setp(ax4.get_xticklabels(), rotation=0, horizontalalignment='center')
+    plt.setp(ax5.get_xticklabels(), rotation=0, horizontalalignment='center')
+
 
     fig.tight_layout()
     fig.subplots_adjust(hspace=0.3)
@@ -250,23 +279,23 @@ def monthly_compare():
                 if 'total_elevation_feet' in week_dict[week][activity]:
                     ele_list.append(float(week_dict[week][activity]['total_elevation_feet']))
                     ele_dict[get_time.FOM(week)] = sum(ele_list)#/len(ele_list)
-                else:
-                    ele_dict[get_time.FOM(week)] = 0
+                # else:
+                #     ele_dict[get_time.FOM(week)] = 0
                 if 'treadmill_flagged' in week_dict[week][activity]:
                     if week_dict[week][activity]['treadmill_flagged'] == 'yes':
                         tred_list.append(1)
-                else:
-                    tred_list.append(0)
+                # else:
+                #     tred_list.append(0)
             hr_dict[get_time.FOM(week)] = sum(hr_list)/len(hr_list)
             miles_dict[get_time.FOM(week)] = sum(mile_list)
             pace_dict[get_time.FOM(week)] = sum(pace_list)/len(pace_list)
             tred_dict[get_time.FOM(week)] = sum(tred_list)
             count_dict[get_time.FOM(week)] = sum(count_list)
-        else:
-            miles_dict[get_time.FOM(week)] = 0
-            pace_dict[get_time.FOM(week)] = 0
-            hr_dict[get_time.FOM(week)] = 0
-            count_dict[get_time.FOM(week)] = 0
+        # else:
+        #     miles_dict[get_time.FOM(week)] = 0
+        #     pace_dict[get_time.FOM(week)] = 0
+        #     hr_dict[get_time.FOM(week)] = 0
+        #     count_dict[get_time.FOM(week)] = 0
 
     x_list = []
     y_list = []
@@ -305,7 +334,7 @@ def monthly_compare():
         y6_list.append(count_dict[month])
 
     ########
-    fig, (ax1,ax2,ax4,ax5) = plt.subplots(nrows=4, figsize=mass_figsize) #figsize sets window
+    fig, (ax1,ax2,ax4,ax5) = plt.subplots(nrows=4, figsize=(13,8)) #figsize sets window
 
     myFmt = mdates.DateFormatter('%m/%y')
 
@@ -319,6 +348,9 @@ def monthly_compare():
     ax1.yaxis.grid(True)
     ax1.legend()
     ax1.set_xticks(x_list)
+    labels = ax1.set_xticklabels(x_list)
+    for i, label in enumerate(labels):
+        label.set_y(label.get_position()[1] - (i % 2) * 0.09)
     ax1.xaxis.set_major_formatter(myFmt)
 
     ax2.plot(x2_list,y2_list, color='g', label='Pace', linewidth=2)
@@ -330,6 +362,9 @@ def monthly_compare():
     ax3.set_ylabel('Avg of Avg HR', color='r')
     ax3.tick_params('y', colors='r')
     ax2.set_xticks(x2_list)
+    labels = ax2.set_xticklabels(x2_list)
+    for i, label in enumerate(labels):
+        label.set_y(label.get_position()[1] - (i % 2) * 0.09)
     ax2.xaxis.set_major_formatter(myFmt)
 
     ax4.bar(x6_list,y6_list, align='center', width=25, color='b', label='Outdoor') #total runs
@@ -340,6 +375,9 @@ def monthly_compare():
     ax4.yaxis.grid(True)
     ax4.legend()
     ax4.set_xticks(x5_list)
+    labels = ax4.set_xticklabels(x6_list)
+    for i, label in enumerate(labels):
+        label.set_y(label.get_position()[1] - (i % 2) * 0.09)
     ax4.xaxis.set_major_formatter(myFmt)
 
     ax5.plot(x4_list,y4_list, label='Total')
@@ -348,7 +386,15 @@ def monthly_compare():
     ax5.yaxis.grid(True)
     ax5.legend()
     ax5.set_xticks(x4_list)
+    labels = ax5.set_xticklabels(x4_list)
+    for i, label in enumerate(labels):
+        label.set_y(label.get_position()[1] - (i % 2) * 0.09)
     ax5.xaxis.set_major_formatter(myFmt)
+
+    plt.setp(ax1.get_xticklabels(), rotation=0, horizontalalignment='center')
+    plt.setp(ax2.get_xticklabels(), rotation=0, horizontalalignment='center')
+    plt.setp(ax4.get_xticklabels(), rotation=0, horizontalalignment='center')
+    plt.setp(ax5.get_xticklabels(), rotation=0, horizontalalignment='center')
 
     fig.tight_layout()
     fig.subplots_adjust(hspace=0.3)
